065d28ce0650053eee51ca2f34cf4c8b2a1d56e9
Zimil-Patel <patelzimil2310@gmail.com>
2025-03-01 12:43:26 +0530

    Firebase Auth setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'dart:developer';
+
 import 'package:emp_system/firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 Future<void> main() async {
   // THIS ENSURES FIREBASE INITIALIZED BEFORE APP RUNS
@@ -22,9 +26,40 @@ class EmployeeSystem extends StatelessWidget {
           title: Text('Home'),
         ),
         body: Center(
-          child: ElevatedButton(
-            onPressed: null,
-            child: Text('Sign in with google'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () async {
+                 try{
+                   final signInAct = await GoogleSignIn().signIn();
+                   final googleAuth = await signInAct?.authentication;
+                   if(googleAuth != null){
+                     final credential = GoogleAuthProvider.credential(
+                       accessToken: googleAuth.accessToken,
+                       idToken: googleAuth.idToken,
+                     );
+                     await FirebaseAuth.instance.signInWithCredential(credential);
+                   }
+                 } catch(e) {
+                   log("Exception : $e");
+                 }
+                },
+                child: Text('Sign in with google'),
+              ),
+
+              // SIGN OUT
+              ElevatedButton(
+                onPressed: () async {
+                  try{
+                    await FirebaseAuth.instance.signOut();
+                  } catch(e) {
+                    log("Exception : $e");
+                  }
+                },
+                child: Text('Sign out'),
+              ),
+            ],
           ),
         ),
       ),
